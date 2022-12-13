8edb5d10b48e4b750dea78ca17f44af876a0e7e2
john2k2 <ortiz.jonathan@live.com>
2022-12-13 13:39:56 -0300

    aplicando scraping

--- src/pages/PaginaEmision.jsx
+++ src/pages/PaginaEmision.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import cheerio from "cheerio";
 
 const PaginaEmision = () => {
-  return <div></div>;
-};
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("https://monoschinos2.com");
+      const $ = cheerio.load(result.data);
+
+      // Aquí puedes usar cheerio para analizar y manipular el HTML de la página web
+      // y extraer los datos que necesitas
+      const data = $(
+        "body > section:nth-child(4) > div > div > div.row.row-cols-5"
+      ).html(); // <--- Aquí extraigo el HTML de la clase animeimgdiv
+
+      const img = $(".animeimgdiv").find("img").attr("data-src"); // <--- Aquí extraigo el src de la imagen
+
+      const title = $(".animeimgdiv").find("img").attr("alt"); // <--- Aquí extraigo el alt de la imagen
+
+      const status = $(".animeimgdiv").find("img").attr("data-status"); // <--- Aquí extraigo el status de la imagen
+
+      const cap = $(
+        "body > section:nth-child(4) > div > div > div.row.row-cols-5 > div:nth-child(2) > a > div > div > div > div > p"
+      ).text(); // <--- Aquí extraigo el texto de la clase overdiv
+
+      const button = $(
+        "body > section:nth-child(4) > div > div > div.row.row-cols-5 > div:nth-child(2) > a > div > div > div > div > button"
+      ).text(); // <--- Aquí extraigo el texto del botón "Ver más
+
+      setData({ data, img, title, cap, status, button });
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-white">Encontrar</h1>
+      <h2 className="text-white">{data.title}</h2>
+      <div>
+        <div>
+          <img src={data.img} alt={data.title} />
+          <h3 className="text-white">{data.title}</h3>
+          <p className="text-white">{data.cap}</p>
+          <button className="text-white ">{data.button}</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default PaginaEmision;
--- src/pages/PaginaMovies.jsx
+++ src/pages/PaginaMovies.jsx
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import cheerio from "cheerio";
 
 const PaginaMovies = () => {
+  const [data, setData] = useState([]);
+  const [img, setImg] = useState([]);
+
+  const url = "https://monoschinos2.com";
+
+  useEffect(() => {
+    const getdata = async () => {
+      const result = await axios.get(url);
+      const $ = cheerio.load(result.data);
+
+      $(".animeimgdiv").each((i, el) => {
+        const card = $(el).html();
+        const alt = $(el).find("img").attr("alt");
+        const cap = $(el).find("p").text();
+        const button = $(el).find("button").text();
+        const img = $(el).find("img").attr("data-src");
+        const status = $(el).find("img").attr("data-status");
+        const title = $(el).find(".animetitles").text();
+
+        setData((data) => [
+          ...data,
+          { card, alt, cap, button, title, img, status },
+        ]);
+      });
+    };
+    getdata();
+  }, []);
+
   return (
     <div>
-      <h1>Movies</h1>
+      <div className="mx-auto w-[80vw] ">
+        <div className="flex flex-wrap gap-8">
+          {data.map((card) => (
+            <div
+              className="relative mx-auto max-w-[200px] transition-all duration-300 ease-in-out hover:scale-[1.05] "
+              key={card.id}
+            >
+              <img
+                className=" w-full rounded-md  object-cover shadow-md "
+                src={card.img}
+                alt={card.alt}
+              />
+              <h3 className="w-full text-center text-white">{card.title}</h3>
+              <p className=" absolute top-4 right-4 text-white">{card.cap}</p>
+              <button className="absolute bottom-[4.5rem] left-4 text-white ">
+                {card.button}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
-
 export default PaginaMovies;
